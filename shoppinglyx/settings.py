2f06e077b656336377173e49ff5463161bb5266d
Rukhsar Khan <rukhsarmev07@gmail.com>
2021-02-25 12:53:47 +0530

    er

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -129,9 +129,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'media'
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'media'
+# ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 # STATIC_ROOT = BASE_DIR / 'static'
